cfeeb5a6bafeb17235b2db1897107a6989896334
Nnaemeka Okalla <emyokalla@gmail.com>
2024-09-14 00:44:23 +0100

    Setting up userRoutes and userController functions_handling and testing auth

---
+++ backend/controllers/userController.js
@@ -0,0 +1,102 @@
+import User from "../models/userModel.js";
+import Post from "../models/postModel.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
+// import { v2 as cloudinary } from "cloudinary";
+// import mongoose from "mongoose";
+
+// const getUserProfile = async (req, res) => {
+	// We will fetch user profile either with username or userId
+	// query is either username or userId
+	// const { query } = req.params;
+
+// 	try {
+// 		let user;
+
+// 		// query is userId
+// 		if (mongoose.Types.ObjectId.isValid(query)) {
+// 			user = await User.findOne({ _id: query }).select("-password").select("-updatedAt");
+// 		} else {
+// 			// query is username
+// 			user = await User.findOne({ username: query }).select("-password").select("-updatedAt");
+// 		}
+
+// 		if (!user) return res.status(404).json({ error: "User not found" });
+
+// 		res.status(200).json(user);
+// 	} catch (err) {
+// 		res.status(500).json({ error: err.message });
+// 		console.log("Error in getUserProfile: ", err.message);
+// 	}
+// };
+
+const signupUser = async (req, res) => {
+	try {
+		const { name, email, username, password } = req.body;
+		const user = await User.findOne({ $or: [{ email }, { username }] });
+
+		if (user) {
+			return res.status(400).json({ error: "User already exists" });
+		}
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			name,
+			email,
+			username,
+			password: hashedPassword,
+		});
+		await newUser.save();
+
+		if (newUser) {
+			generateTokenAndSetCookie(newUser._id, res);
+
+			res.status(201).json({
+				_id: newUser._id,
+				name: newUser.name,
+				email: newUser.email,
+				username: newUser.username,
+				bio: newUser.bio,
+				profilePic: newUser.profilePic,
+			});
+		} else {
+			res.status(400).json({ error: "Invalid user data" });
+		}
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log("Error in signupUser: ", err.message);
+	}
+};
+
+const loginUser = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+		if (!user || !isPasswordCorrect) return res.status(400).json({ error: "Invalid username or password" });
+
+		if (user.isFrozen) {
+			user.isFrozen = false;
+			await user.save();
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			username: user.username,
+			bio: user.bio,
+			profilePic: user.profilePic,
+		});
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+		console.log("Error in loginUser: ", error.message);
+	}
+};
+
+
+export { signupUser, loginUser };

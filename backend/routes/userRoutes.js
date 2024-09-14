afad62bd77e466b06c8ad58812db25b8059dce16
Nnaemeka Okalla <emyokalla@gmail.com>
2024-09-14 02:23:41 +0100

    handles userRoutes and controllers initializing postRoutes and updating postControllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,25 +1,25 @@
 import express from "express";
 import {
-	// followUnFollowUser,
-	// getUserProfile,
+	followUnFollowUser,
+	getUserProfile,
 	loginUser,
-	// logoutUser,
+	logoutUser,
 	signupUser,
-	// updateUser,
+	updateUser,
 	// getSuggestedUsers,
 	// freezeAccount,
 } from "../controllers/userController.js";
-// import protectRoute from "../middlewares/protectRoute.js";
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
-// router.get("/profile/:query", getUserProfile);
+router.get("/profile/:username", getUserProfile);
 // router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
-// router.post("/logout", logoutUser);
-// router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
-// router.put("/update/:id", protectRoute, updateUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
+router.put("/update/:id", protectRoute, updateUser);
 // router.put("/freeze", protectRoute, freezeAccount);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './db/connectDB.js';
 import cookieParser from 'cookie-parser';
 
 import userRoutes from './routes/userRoutes.js';
+import postRoutes from './routes/postRoutes.js';
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ app.use(cookieParser());
 
 //Routes
 app.use("/api/users", userRoutes);
-// app.use("/api/posts", postRoutes);
+app.use("/api/posts", postRoutes);
 // app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => 

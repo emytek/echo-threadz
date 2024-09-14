961dd17f839d02823bccc0a50239e57f3359b917
Nnaemeka Okalla <emyokalla@gmail.com>
2024-09-14 13:59:46 +0100

    handled auth integration with user state management and error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,28 @@
 import { Box, Container } from "@chakra-ui/react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
-import { Button } from "@chakra-ui/react";
-import ChatPage from "./pages/ChatPage";
-import PostPage from "./pages/PostPage";
 import UserPage from "./pages/UserPage";
-import CreatePost from "./components/CreatePost";
+import PostPage from "./pages/PostPage";
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
+import { useRecoilValue } from "recoil";
+import userAtom from "./atoms/userAtom";
 import UpdateProfilePage from "./pages/UpdateProfilePage";
-import Header from "./components/Header";
-import SettingsPage from "./pages/SettingsPage";
+import CreatePost from "./components/CreatePost";
+import ChatPage from "./pages/ChatPage";
+import  SettingsPage  from "./pages/SettingsPage";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
-  const user = true;
+	const user = useRecoilValue(userAtom);
 	const { pathname } = useLocation();
-
-  return (
-    <>
-      <Box position={"relative"} w='full'>
+	return (
+		<Box position={"relative"} w='full'>
 			<Container maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}>
 				<Header />
 				<Routes>
-					{/* <Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} /> */}
-					<Route path='/' element={<HomePage />} />
-					{/* <Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} /> */}
-					<Route path='/auth' element={ <AuthPage />} />
+					<Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
+					<Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
 					<Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
 
 					<Route
@@ -42,12 +40,13 @@ function App() {
 					/>
 					<Route path='/:username/post/:pid' element={<PostPage />} />
 					<Route path='/chat' element={user ? <ChatPage /> : <Navigate to={"/auth"} />} />
-					<Route path='/settings' element={user ? <SettingsPage /> : <Navigate to={"/auth"} />} />
+					{/* <Route path='/settings' element={user ? <SettingsPage /> : <Navigate to={"/auth"} />} /> */}
 				</Routes>
+
+				{ user && <LogoutButton />}
 			</Container>
 		</Box>
-    </>
-  )
+	);
 }
 
-export default App
+export default App;

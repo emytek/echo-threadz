89136b9c3fa7b3b810b45b77a2fdda88ead8cc86
Nnaemeka Okalla <emyokalla@gmail.com>
2024-09-14 03:04:55 +0100

    updated get_create_delete and like post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -40,71 +40,71 @@ const createPost = async (req, res) => {
 	}
 };
 
-// const getPost = async (req, res) => {
-// 	try {
-// 		const post = await Post.findById(req.params.id);
+const getPost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
 
-// 		if (!post) {
-// 			return res.status(404).json({ error: "Post not found" });
-// 		}
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
 
-// 		res.status(200).json(post);
-// 	} catch (err) {
-// 		res.status(500).json({ error: err.message });
-// 	}
-// };
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
 
-// const deletePost = async (req, res) => {
-// 	try {
-// 		const post = await Post.findById(req.params.id);
-// 		if (!post) {
-// 			return res.status(404).json({ error: "Post not found" });
-// 		}
+const deletePost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
 
-// 		if (post.postedBy.toString() !== req.user._id.toString()) {
-// 			return res.status(401).json({ error: "Unauthorized to delete post" });
-// 		}
+		if (post.postedBy.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ error: "Unauthorized to delete post" });
+		}
 
-// 		if (post.img) {
-// 			const imgId = post.img.split("/").pop().split(".")[0];
-// 			await cloudinary.uploader.destroy(imgId);
-// 		}
+		// if (post.img) {
+		// 	const imgId = post.img.split("/").pop().split(".")[0];
+		// 	await cloudinary.uploader.destroy(imgId);
+		// }
 
-// 		await Post.findByIdAndDelete(req.params.id);
+		await Post.findByIdAndDelete(req.params.id);
 
-// 		res.status(200).json({ message: "Post deleted successfully" });
-// 	} catch (err) {
-// 		res.status(500).json({ error: err.message });
-// 	}
-// };
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
 
-// const likeUnlikePost = async (req, res) => {
-// 	try {
-// 		const { id: postId } = req.params;
-// 		const userId = req.user._id;
+const likeUnlikePost = async (req, res) => {
+	try {
+		const { id: postId } = req.params;
+		const userId = req.user._id;
 
-// 		const post = await Post.findById(postId);
+		const post = await Post.findById(postId);
 
-// 		if (!post) {
-// 			return res.status(404).json({ error: "Post not found" });
-// 		}
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
 
-// 		const userLikedPost = post.likes.includes(userId);
+		const userLikedPost = post.likes.includes(userId);
 
-// 		if (userLikedPost) {
-// 			// Unlike post
-// 			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
-// 			res.status(200).json({ message: "Post unliked successfully" });
-// 		} else {
-// 			// Like post
-// 			post.likes.push(userId);
-// 			await post.save();
-// 			res.status(200).json({ message: "Post liked successfully" });
-// 		}
-// 	} catch (err) {
-// 		res.status(500).json({ error: err.message });
-// 	}
-// };
+		if (userLikedPost) {
+			// Unlike post
+			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+			res.status(200).json({ message: "Post unliked successfully" });
+		} else {
+			// Like post
+			post.likes.push(userId);
+			await post.save();
+			res.status(200).json({ message: "Post liked successfully" });
+		}
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
 
 // const replyToPost = async (req, res) => {
 // 	try {
@@ -168,4 +168,4 @@ const createPost = async (req, res) => {
 // 	}
 // };
 
-export { createPost };
+export { createPost, getPost, deletePost, likeUnlikePost };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {
 	createPost,
-	// deletePost,
-	// getPost,
-	// likeUnlikePost,
+	deletePost,
+	getPost,
+	likeUnlikePost,
 	// replyToPost,
 	// getFeedPosts,
 	// getUserPosts,
@@ -13,11 +13,11 @@ import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 // router.get("/feed", protectRoute, getFeedPosts);
-// router.get("/:id", getPost);
+router.get("/:id", getPost);
 // router.get("/user/:username", getUserPosts);
 router.post("/create", protectRoute, createPost);
-// router.delete("/:id", protectRoute, deletePost);
-// router.put("/like/:id", protectRoute, likeUnlikePost);
+router.delete("/:id", protectRoute, deletePost);
+router.put("/like/:id", protectRoute, likeUnlikePost);
 // router.put("/reply/:id", protectRoute, replyToPost);
 
 export default router;

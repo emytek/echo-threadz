7cf362f3bea21b2933879335696262baeb059f7a
Nnaemeka Okalla <emyokalla@gmail.com>
2024-09-20 09:22:17 +0100

    handling messages and conversations on the client

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -7,7 +7,8 @@ import { conversationsAtom, selectedConversationAtom } from "../atoms/messagesAt
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
 import { useSocket } from "../context/SocketContext.jsx";
-import messageSound from "../assets/sounds/message.mp3";
+// import messageSound from "../assets/sounds/message.mp3";
+
 const MessageContainer = () => {
 	const showToast = useShowToast();
 	const selectedConversation = useRecoilValue(selectedConversationAtom);
@@ -18,91 +19,91 @@ const MessageContainer = () => {
 	const setConversations = useSetRecoilState(conversationsAtom);
 	const messageEndRef = useRef(null);
 
-	// useEffect(() => {
-	// 	socket.on("newMessage", (message) => {
-	// 		if (selectedConversation._id === message.conversationId) {
-	// 			setMessages((prev) => [...prev, message]);
-	// 		}
+	useEffect(() => {
+		socket.on("newMessage", (message) => {
+			if (selectedConversation._id === message.conversationId) {
+				setMessages((prev) => [...prev, message]);
+			}
 
-	// 		// make a sound if the window is not focused
-	// 		if (!document.hasFocus()) {
-	// 			const sound = new Audio(messageSound);
-	// 			sound.play();
-	// 		}
+			// make a sound if the window is not focused
+			// if (!document.hasFocus()) {
+			// 	const sound = new Audio(messageSound);
+			// 	sound.play();
+			// }
 
-	// 		setConversations((prev) => {
-	// 			const updatedConversations = prev.map((conversation) => {
-	// 				if (conversation._id === message.conversationId) {
-	// 					return {
-	// 						...conversation,
-	// 						lastMessage: {
-	// 							text: message.text,
-	// 							sender: message.sender,
-	// 						},
-	// 					};
-	// 				}
-	// 				return conversation;
-	// 			});
-	// 			return updatedConversations;
-	// 		});
-	// 	});
+			setConversations((prev) => {
+				const updatedConversations = prev.map((conversation) => {
+					if (conversation._id === message.conversationId) {
+						return {
+							...conversation,
+							lastMessage: {
+								text: message.text,
+								sender: message.sender,
+							},
+						};
+					}
+					return conversation;
+				});
+				return updatedConversations;
+			});
+		});
 
-	// 	return () => socket.off("newMessage");
-	// }, [socket, selectedConversation, setConversations]);
+		return () => socket.off("newMessage");
+	}, [socket, selectedConversation, setConversations]);
 
-	// useEffect(() => {
-	// 	const lastMessageIsFromOtherUser = messages.length && messages[messages.length - 1].sender !== currentUser._id;
-	// 	if (lastMessageIsFromOtherUser) {
-	// 		socket.emit("markMessagesAsSeen", {
-	// 			conversationId: selectedConversation._id,
-	// 			userId: selectedConversation.userId,
-	// 		});
-	// 	}
+	useEffect(() => {
+		const lastMessageIsFromOtherUser = messages.length && messages[messages.length - 1].sender !== currentUser._id;
+		if (lastMessageIsFromOtherUser) {
+			socket.emit("markMessagesAsSeen", {
+				conversationId: selectedConversation._id,
+				userId: selectedConversation.userId,
+			});
+		}
 
-	// 	socket.on("messagesSeen", ({ conversationId }) => {
-	// 		if (selectedConversation._id === conversationId) {
-	// 			setMessages((prev) => {
-	// 				const updatedMessages = prev.map((message) => {
-	// 					if (!message.seen) {
-	// 						return {
-	// 							...message,
-	// 							seen: true,
-	// 						};
-	// 					}
-	// 					return message;
-	// 				});
-	// 				return updatedMessages;
-	// 			});
-	// 		}
-	// 	});
-	// }, [socket, currentUser._id, messages, selectedConversation]);
+		socket.on("messagesSeen", ({ conversationId }) => {
+			if (selectedConversation._id === conversationId) {
+				setMessages((prev) => {
+					const updatedMessages = prev.map((message) => {
+						if (!message.seen) {
+							return {
+								...message,
+								seen: true,
+							};
+						}
+						return message;
+					});
+					return updatedMessages;
+				});
+			}
+		});
+	}, [socket, currentUser._id, messages, selectedConversation]);
 
-	// useEffect(() => {
-	// 	messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
-	// }, [messages]);
+	useEffect(() => {
+		messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+	}, [messages]);
 
-	// useEffect(() => {
-	// 	const getMessages = async () => {
-	// 		setLoadingMessages(true);
-	// 		setMessages([]);
-	// 		try {
-	// 			if (selectedConversation.mock) return;
-	// 			const res = await fetch(`/api/messages/${selectedConversation.userId}`);
-	// 			const data = await res.json();
-	// 			if (data.error) {
-	// 				showToast("Error", data.error, "error");
-	// 				return;
-	// 			}
-	// 			setMessages(data);
-	// 		} catch (error) {
-	// 			showToast("Error", error.message, "error");
-	// 		} finally {
-	// 			setLoadingMessages(false);
-	// 		}
-	// 	};
+	useEffect(() => {
+		const getMessages = async () => {
+			setLoadingMessages(true);
+			setMessages([]);
+			try {
+				if (selectedConversation.mock) return;
+				const res = await fetch(`/api/messages/${selectedConversation.userId}`);
+				const data = await res.json();
+				if (data.error) {
+					showToast("Error", data.error, "error");
+					return;
+				}
+				setMessages(data);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setLoadingMessages(false);
+			}
+		};
 
-	// 	getMessages();
-	// }, [showToast, selectedConversation.userId, selectedConversation.mock]);
+		getMessages();
+	}, [showToast, selectedConversation.userId, selectedConversation.mock]);
 
 	return (
 		<Flex
@@ -114,22 +115,17 @@ const MessageContainer = () => {
 		>
 			{/* Message header */}
 			<Flex w={"full"} h={12} alignItems={"center"} gap={2}>
-				<Avatar 
-                // src={selectedConversation.userProfilePic} 
-                src='/client.jpg'
-                size={"sm"} 
-                />
+				<Avatar src={selectedConversation.userProfilePic} size={"sm"} />
 				<Text display={"flex"} alignItems={"center"}>
-					{/* {selectedConversation.username}  */} username
-                    <Image src='/verified.png' w={4} h={4} ml={1} />
+					{selectedConversation.username} <Image src='/verified.png' w={4} h={4} ml={1} />
 				</Text>
 			</Flex>
 
 			<Divider />
 
 			<Flex flexDir={"column"} gap={4} my={4} p={2} height={"400px"} overflowY={"auto"}>
-				{/* {loadingMessages &&
-					[...Array(5)].map((_, i) => ( */}
+				{loadingMessages &&
+					[...Array(5)].map((_, i) => (
 						<Flex
 							key={i}
 							gap={2}
@@ -146,10 +142,10 @@ const MessageContainer = () => {
 							</Flex>
 							{i % 2 !== 0 && <SkeletonCircle size={7} />}
 						</Flex>
-					{/* ))} */}
+					))}
 
-				{/* {!loadingMessages &&
-					messages.map((message) => ( */}
+				{!loadingMessages &&
+					messages.map((message) => (
 						<Flex
 							key={message._id}
 							direction={"column"}
@@ -157,7 +153,7 @@ const MessageContainer = () => {
 						>
 							<Message message={message} ownMessage={currentUser._id === message.sender} />
 						</Flex>
-					{/*))}*/}
+					))}
 			</Flex>
 
 			<MessageInput setMessages={setMessages} />
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,8 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import { Box, Button, Flex, Input, Skeleton, SkeletonCircle, Text, useColorModeValue } from "@chakra-ui/react";
-// import Conversation from "../components/Conversation";
+import Conversation from "../components/Conversation";
 import { GiConversation } from "react-icons/gi";
-// import MessageContainer from "../components/MessageContainer";
+import MessageContainer from "../components/MessageContainer";
 import { useEffect, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import { useRecoilState, useRecoilValue } from "recoil";
@@ -14,106 +14,106 @@ const ChatPage = () => {
 	const [searchingUser, setSearchingUser] = useState(false);
 	const [loadingConversations, setLoadingConversations] = useState(true);
 	const [searchText, setSearchText] = useState("");
-	// const [selectedConversation, setSelectedConversation] = useRecoilState(selectedConversationAtom);
-	// const [conversations, setConversations] = useRecoilState(conversationsAtom);
+	const [selectedConversation, setSelectedConversation] = useRecoilState(selectedConversationAtom);
+	const [conversations, setConversations] = useRecoilState(conversationsAtom);
 	const currentUser = useRecoilValue(userAtom);
 	const showToast = useShowToast();
 	const { socket, onlineUsers } = useSocket();
 
-	// useEffect(() => {
-	// 	socket?.on("messagesSeen", ({ conversationId }) => {
-	// 		setConversations((prev) => {
-	// 			const updatedConversations = prev.map((conversation) => {
-	// 				if (conversation._id === conversationId) {
-	// 					return {
-	// 						...conversation,
-	// 						lastMessage: {
-	// 							...conversation.lastMessage,
-	// 							seen: true,
-	// 						},
-	// 					};
-	// 				}
-	// 				return conversation;
-	// 			});
-	// 			return updatedConversations;
-	// 		});
-	// 	});
-	// }, [socket, setConversations]);
+	useEffect(() => {
+		socket?.on("messagesSeen", ({ conversationId }) => {
+			setConversations((prev) => {
+				const updatedConversations = prev.map((conversation) => {
+					if (conversation._id === conversationId) {
+						return {
+							...conversation,
+							lastMessage: {
+								...conversation.lastMessage,
+								seen: true,
+							},
+						};
+					}
+					return conversation;
+				});
+				return updatedConversations;
+			});
+		});
+	}, [socket, setConversations]);
 
-	// useEffect(() => {
-	// 	const getConversations = async () => {
-	// 		try {
-	// 			const res = await fetch("/api/messages/conversations");
-	// 			const data = await res.json();
-	// 			if (data.error) {
-	// 				showToast("Error", data.error, "error");
-	// 				return;
-	// 			}
-	// 			console.log(data);
-	// 			setConversations(data);
-	// 		} catch (error) {
-	// 			showToast("Error", error.message, "error");
-	// 		} finally {
-	// 			setLoadingConversations(false);
-	// 		}
-	// 	};
+	useEffect(() => {
+		const getConversations = async () => {
+			try {
+				const res = await fetch("/api/messages/conversations");
+				const data = await res.json();
+				if (data.error) {
+					showToast("Error", data.error, "error");
+					return;
+				}
+				console.log(data);
+				setConversations(data);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setLoadingConversations(false);
+			}
+		};
 
-	// 	getConversations();
-	// }, [showToast, setConversations]);
+		getConversations();
+	}, [showToast, setConversations]);
 
-	// const handleConversationSearch = async (e) => {
-	// 	e.preventDefault();
-	// 	setSearchingUser(true);
-	// 	try {
-	// 		const res = await fetch(`/api/users/profile/${searchText}`);
-	// 		const searchedUser = await res.json();
-	// 		if (searchedUser.error) {
-	// 			showToast("Error", searchedUser.error, "error");
-	// 			return;
-	// 		}
+	const handleConversationSearch = async (e) => {
+		e.preventDefault();
+		setSearchingUser(true);
+		try {
+			const res = await fetch(`/api/users/profile/${searchText}`);
+			const searchedUser = await res.json();
+			if (searchedUser.error) {
+				showToast("Error", searchedUser.error, "error");
+				return;
+			}
 
-	// 		const messagingYourself = searchedUser._id === currentUser._id;
-	// 		if (messagingYourself) {
-	// 			showToast("Error", "You cannot message yourself", "error");
-	// 			return;
-	// 		}
+			const messagingYourself = searchedUser._id === currentUser._id;
+			if (messagingYourself) {
+				showToast("Error", "You cannot message yourself", "error");
+				return;
+			}
 
-	// 		const conversationAlreadyExists = conversations.find(
-	// 			(conversation) => conversation.participants[0]._id === searchedUser._id
-	// 		);
+			const conversationAlreadyExists = conversations.find(
+				(conversation) => conversation.participants[0]._id === searchedUser._id
+			);
 
-	// 		if (conversationAlreadyExists) {
-	// 			setSelectedConversation({
-	// 				_id: conversationAlreadyExists._id,
-	// 				userId: searchedUser._id,
-	// 				username: searchedUser.username,
-	// 				userProfilePic: searchedUser.profilePic,
-	// 			});
-	// 			return;
-	// 		}
+			if (conversationAlreadyExists) {
+				setSelectedConversation({
+					_id: conversationAlreadyExists._id,
+					userId: searchedUser._id,
+					username: searchedUser.username,
+					userProfilePic: searchedUser.profilePic,
+				});
+				return;
+			}
 
-	// 		const mockConversation = {
-	// 			mock: true,
-	// 			lastMessage: {
-	// 				text: "",
-	// 				sender: "",
-	// 			},
-	// 			_id: Date.now(),
-	// 			participants: [
-	// 				{
-	// 					_id: searchedUser._id,
-	// 					username: searchedUser.username,
-	// 					profilePic: searchedUser.profilePic,
-	// 				},
-	// 			],
-	// 		};
-	// 		setConversations((prevConvs) => [...prevConvs, mockConversation]);
-	// 	} catch (error) {
-	// 		showToast("Error", error.message, "error");
-	// 	} finally {
-	// 		setSearchingUser(false);
-	// 	}
-	// };
+			const mockConversation = {
+				mock: true,
+				lastMessage: {
+					text: "",
+					sender: "",
+				},
+				_id: Date.now(),
+				participants: [
+					{
+						_id: searchedUser._id,
+						username: searchedUser.username,
+						profilePic: searchedUser.profilePic,
+					},
+				],
+			};
+			setConversations((prevConvs) => [...prevConvs, mockConversation]);
+		} catch (error) {
+			showToast("Error", error.message, "error");
+		} finally {
+			setSearchingUser(false);
+		}
+	};
 
 	return (
 		<Box
@@ -136,25 +136,18 @@ const ChatPage = () => {
 					<Text fontWeight={700} color={useColorModeValue("gray.600", "gray.400")}>
 						Your Conversations
 					</Text>
-					<form 
-          // onSubmit={handleConversationSearch}
-          >
+					<form onSubmit={handleConversationSearch}>
 						<Flex alignItems={"center"} gap={2}>
 							<Input placeholder='Search for a user' onChange={(e) => setSearchText(e.target.value)} />
-							<Button size={"sm"} 
-              // onClick={handleConversationSearch} 
-              isLoading={searchingUser}
-              >
+							<Button size={"sm"} onClick={handleConversationSearch} isLoading={searchingUser}>
 								<SearchIcon />
 							</Button>
 						</Flex>
 					</form>
 
-					{/* {loadingConversations && */}
-						{/* [0, 1, 2, 3, 4].map((_, i) => ( */}
-							<Flex 
-              // key={i} 
-              gap={4} alignItems={"center"} p={"1"} borderRadius={"md"}>
+					{loadingConversations &&
+						[0, 1, 2, 3, 4].map((_, i) => (
+							<Flex key={i} gap={4} alignItems={"center"} p={"1"} borderRadius={"md"}>
 								<Box>
 									<SkeletonCircle size={"10"} />
 								</Box>
@@ -163,18 +156,18 @@ const ChatPage = () => {
 									<Skeleton h={"8px"} w={"90%"} />
 								</Flex>
 							</Flex>
-						{/* ))} */}
+						))}
 
-					{/* {!loadingConversations &&
+					{!loadingConversations &&
 						conversations.map((conversation) => (
 							<Conversation
 								key={conversation._id}
 								isOnline={onlineUsers.includes(conversation.participants[0]._id)}
 								conversation={conversation}
 							/>
-						))} */}
+						))}
 				</Flex>
-				{/* {!selectedConversation._id && ( */}
+				{!selectedConversation._id && (
 					<Flex
 						flex={70}
 						borderRadius={"md"}
@@ -187,9 +180,9 @@ const ChatPage = () => {
 						<GiConversation size={100} />
 						<Text fontSize={20}>Select a conversation to start messaging</Text>
 					</Flex>
-				{/* )} */}
+				)}
 
-				{/* {selectedConversation._id && <MessageContainer />} */}
+				{selectedConversation._id && <MessageContainer />}
 			</Flex>
 		</Box>
 	);
